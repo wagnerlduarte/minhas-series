a7de21e9ebedfa905fdd694fbff8a97ae700685b
Wagner Lemos <wagnerlduarte@hotmail.com>
2019-05-11 17:16:52 -0300

    Adição da tela de Login

---
+++ src/login/Login.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react"
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap"
+import "./Login.css"
+
+import auth from '../auth'
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    validateForm() {
+        return this.state.email.length > 0 && this.state.password.length > 0
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        auth.login()
+        this.props.history.push('/')
+    }
+
+    render() {
+        return (
+            <div className="Login">
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="email" size="large">
+                        <FormLabel>Email</FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="password" size="large">
+                        <FormLabel>Password</FormLabel>
+                        <FormControl
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            type="password"
+                        />
+                    </FormGroup>
+                    <Button
+                        block
+                        size="large"
+                        disabled={!this.validateForm()}
+                        type="submit"
+                    >
+                        Login
+                    </Button>
+                </form>
+            </div>
+        )
+    }
+}
