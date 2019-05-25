4dcc6a7a608c6cb66195e42b534a566579d6dbd1
Wagner Lemos <wagnerlduarte@hotmail.com>
2019-05-24 21:14:56 -0300

    Adicionado componente para avalia série

--- src/ui/components/serie-thumbnail/index.jsx
+++ src/ui/components/serie-thumbnail/index.jsx
@@ -4,6 +4,7 @@ import { RootModal, modalTypes } from '../modal';
 
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import Rating from 'react-rating';
 
 import { Api as api } from '../../../services';
 
@@ -27,10 +28,12 @@ class SerieThumbnail extends Component {
                 closeOnClick: true,
                 pauseOnHover: true,
                 draggable: true
-            }
+            },
+            serie: {...this.props.serie}
         }
 
         this.modalClose = this.modalClose.bind(this)
+        this.changeRate = this.changeRate.bind(this)
     }
 
     modalClose() {
@@ -49,26 +52,50 @@ class SerieThumbnail extends Component {
         })
     }
 
+    changeRate(rate){
+
+        api.loadSeries(this.state.serie.id).then((response) => {
+            let serie = response.data;
+            serie.rate = rate;
+            api.editSeries(serie).then(() => {
+                this.setState({
+                    serie: {...this.state.serie,
+                        rate: rate
+                    }
+                })
+                toast.success('Avaliação salva com sucesso!', this.state.toastOptions)
+            })
+        })
+    }
+
     render() {
         return (
             <>
-                <div key={this.props.serie.id} className="item col-xs-4 col-lg-4">
+                <div key={this.state.serie.id} className="item col-xs-4 col-lg-4">
                     <div className="img-thumbnail">
                         <img className="img-fluid group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
                         <div className="caption">
+                            <Rating
+                                initialRating={this.state.serie.rate}
+                                emptySymbol="far fa-heart fa-2x"
+                                placeholderSymbol="fas fa-heart fa-2x text-danger"
+                                fullSymbol="fas fa-heart fa-2x text-danger"
+                                fractions={2}
+                                onChange={this.changeRate}
+                            />
                             <h4 className="group inner list-group-item-heading">
-                                {this.props.serie.name}
+                                {this.state.serie.name}
                             </h4>
                             <div className="row">
                                 <div className="col-xs-12 col-md-6">
                                     <p className="lead">
-                                        {statuses[this.props.serie.status]}
+                                        {statuses[this.state.serie.status]}
                                     </p>
                                 </div>
                                 {this.props.showButtons &&
                                     <div className="col-xs-12 col-md-6">
-                                        <Link className="btn btn-success" to={`/serie/${this.props.serie.id}`}>Editar</Link>
-                                        <button type="button" className="btn btn-success" onClick={() => this.modalOpen(this.props.serie.id)}>Excluir</button>
+                                        <Link className="btn btn-success" to={`/serie/${this.state.serie.id}`}>Editar</Link>
+                                        <button type="button" className="btn btn-success" onClick={() => this.modalOpen(this.state.serie.id)}>Excluir</button>
                                     </div>
                                 }
                             </div>
