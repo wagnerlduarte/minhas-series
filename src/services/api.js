f153b05f23c070efa5f4599ca2d6e51b6a942b5b
Wagner Lemos <wagnerlduarte@hotmail.com>
2020-04-01 23:34:44 -0300

    fake login

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,26 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = 'http://localhost:3001/';
-// const baseURL = '';//'http://192.168.99.100:3001/';
-const strapiURL = 'http://localhost:1337/';
+const baseURL = "http://localhost:3001/";
 
-const genres = ['Comédia', 'Drama', 'Ação'];
+const genres = ["Comédia", "Drama", "Ação"];
 
 const apis = {
-    // loadGenres: () => axios.get(`${baseURL}genres`),
-    loadGenres: () => Promise.resolve({ data: genres }),
-    loadSeriesByGenre: (genreName, limit) => axios.get(`${baseURL}series?genre=${genreName}` + (limit ? '&_limit=' + limit : '')),
-    loadSeries: (id) => axios.get(`${baseURL}serie/${id}`),
-    deleteSeries: (id) => axios.delete(`${baseURL}serie/${id}`),
-    saveSeries: (newSerie) => axios.post(`${baseURL}serie`, newSerie),
-    editSeries: (editedSerie) => axios.put(`${baseURL}serie/${editedSerie.id}`, editedSerie),
-    getUnratedSeries: () => axios.get(`${baseURL}series?rate=0`),
-    login: (user) => axios.post(`${strapiURL}auth/local`, { identifier: user.email, password: user.password }),
-    callbackProvider: (provider, search) => axios.get(`${strapiURL}auth/${provider}/callback${search}`)
-}
+  loadGenres: () => Promise.resolve({ data: genres }),
+  loadSeriesByGenre: (genreName, limit) =>
+    axios.get(
+      `${baseURL}series?genre=${genreName}` + (limit ? "&_limit=" + limit : "")
+    ),
+  loadSeries: id => axios.get(`${baseURL}serie/${id}`),
+  deleteSeries: id => axios.delete(`${baseURL}serie/${id}`),
+  saveSeries: newSerie => axios.post(`${baseURL}serie`, newSerie),
+  editSeries: editedSerie =>
+    axios.put(`${baseURL}serie/${editedSerie.id}`, editedSerie),
+  getUnratedSeries: () => axios.get(`${baseURL}series?rate=0`),
+  login: user =>
+    axios.post(`${baseURL}login`, {
+      identifier: user.email,
+      password: user.password
+    })
+};
 
 export default apis;
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,84 +1,81 @@
-import Api from './api';
+import Api from "./api";
 
-const TOKEN_KEY = 'jwtToken';
-const USER_INFO = 'userInfo';
+const TOKEN_KEY = "jwtToken";
+const USER_INFO = "userInfo";
 
 const parse = JSON.parse;
 const stringify = JSON.stringify;
 
-const login = (user) => {
-    return Api.login(user).then((response) => {
-        setToken(response.data.jwt, true);
-        setUserInfo(response.data.user, true);
-        return;
+const login = user => {
+  return Api.login(user)
+    .then(response => {
+      setToken(response.data.jwt, true);
+      setUserInfo(response.data.user, true);
+      return;
+    })
+    .catch(error => {
+      alert(error);
     });
-}
+};
 
-const callbackProvider = (provider, search) => {
-    return Api.callbackProvider(provider, search)
-        .then(response => {
-            setToken(response.data.jwt, true);
-            setUserInfo(response.data.user, true);
-            return
-        })
-        .catch(err => {
-            console.log(err);
-        });
-}
 const logout = () => {
-    localStorage.removeItem(TOKEN_KEY);
-    localStorage.removeItem(USER_INFO);
-}
+  localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(USER_INFO);
+};
 
 const set = (value, key, isLocalStorage) => {
-    if (!value) {
-        return null;
-    }
-
-    if (isLocalStorage && localStorage) {
-        return localStorage.setItem(key, stringify(value));
-    }
-
-    // if (sessionStorage) {
-    //     return sessionStorage.setItem(key, stringify(value));
-    // }
-
+  if (!value) {
     return null;
-}
+  }
 
-const setToken = (value = '', isLocalStorage = false, tokenKey = TOKEN_KEY) => {
-    return set(value, tokenKey, isLocalStorage);
-}
+  if (isLocalStorage && localStorage) {
+    return localStorage.setItem(key, stringify(value));
+  }
 
-const setUserInfo = (value = '', isLocalStorage = false, userInfo = USER_INFO) => {
-    return set(value, userInfo, isLocalStorage);
-}
+  // if (sessionStorage) {
+  //     return sessionStorage.setItem(key, stringify(value));
+  // }
 
+  return null;
+};
 
-const get = (key) => {
-    if (localStorage && localStorage.getItem(key)) {
-        return parse(localStorage.getItem(key)) || null;
-    }
+const setToken = (value = "", isLocalStorage = false, tokenKey = TOKEN_KEY) => {
+  return set(value, tokenKey, isLocalStorage);
+};
 
-    // if (sessionStorage && sessionStorage.getItem(key)) {
-    //     return parse(sessionStorage.getItem(key)) || null;
-    // }
+const setUserInfo = (
+  value = "",
+  isLocalStorage = false,
+  userInfo = USER_INFO
+) => {
+  return set(value, userInfo, isLocalStorage);
+};
 
-    return null;
-}
+const get = key => {
+  if (localStorage && localStorage.getItem(key)) {
+    return parse(localStorage.getItem(key)) || null;
+  }
+
+  // if (sessionStorage && sessionStorage.getItem(key)) {
+  //     return parse(sessionStorage.getItem(key)) || null;
+  // }
+
+  return null;
+};
 
 const getToken = (tokenKey = TOKEN_KEY) => {
-    return true;
-    // return get(tokenKey);
-}
+  // return true;
+  return get(tokenKey);
+};
 
 const getUserInfo = (userInfo = USER_INFO) => {
-    return get(userInfo);
-}
+  return get(userInfo);
+};
 
 const isAuthenticated = () => Boolean(getToken());
 
 export default {
-    login, logout, isAuthenticated, callbackProvider
-}
-
+  login,
+  logout,
+  isAuthenticated
+};
